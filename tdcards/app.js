6dedb69dd54e5026b5518f6d9d610f40ea9e5ac7
Colin Fitzpatrick <fitzcn@u.northwestern.edu>
2014-12-15 00:07:07 -0600

    added td cards
    hurray!

---
+++ tdcards/app.js
@@ -0,0 +1,63 @@
+// Ionic Starter App, v0.9.20
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+})
+
+.directive('noScroll', function($document) {
+
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+
+      $document.on('touchmove', function(e) {
+        e.preventDefault();
+      });
+    }
+  }
+})
+
+.controller('CardsCtrl', function($scope, TDCardDelegate) {
+  console.log('CARDS CTRL');
+  var cardTypes = [{
+    title: 'Would you want to work with this person?',
+    image: 'http://distilleryimage8.ak.instagram.com/e17837faafb611e3b8a7126a0592d374_8.jpg'
+  }, {
+    title: 'Would you be friends with this person?',
+    image: 'http://distilleryimage10.ak.instagram.com/d7373e60a22a11e3a110123da7743db2_8.jpg'
+  }, {
+    title: 'Would you go on a date with this person?',
+    image: 'http://photos-d.ak.instagram.com/hphotos-ak-xfa1/917045_730403106982531_1038497188_n.jpg'
+  }];
+
+  $scope.cards = Array.prototype.slice.call(cardTypes, 0);
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+  }
+})
+
+.controller('CardCtrl', function($scope, TDCardDelegate) {
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
+});
